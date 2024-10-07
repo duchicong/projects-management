ac2287b9f37da164c9cd311b627be2a83c070492
CONG.DU <Duconggpdg@gmail.com>
2024-10-07 21:53:03 +0700

    Section 23: adding payments

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PDFDocument = require("pdfkit");
 const Order = require("../models/order");
 const Product = require("../models/product");
+const stripe = require("stripe")("pk_test_secret_key");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -136,18 +137,52 @@ exports.postCart = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/checkout", {
-      prods: products,
-      pageTitle: "Checkout",
-      path: "/checkout",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
-      errorMsg: undefined,
-      validate: [],
+  let products;
+  let total = 0;
+
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+
+      total = products.reduce(
+        (a, b) => (a += b.quantity * b.productId.price),
+        0
+      );
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => ({
+          name: p.productId.title,
+          description: p.productId.description,
+          amount: p.productId.price * 100,
+          currency: "usd",
+          quantity: p.quantity,
+        })),
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success", // http://localhost:3000
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        products,
+        pageTitle: "Checkout",
+        path: "/checkout",
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+        errorMsg: undefined,
+        validate: [],
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
-  });
 };
 
 exports.postDeleteItemCart = (req, res, next) => {
@@ -163,6 +198,40 @@ exports.postDeleteItemCart = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => ({
+        quantity: i.quantity,
+        product: { ...i.productId._doc },
+      }));
+
+      const order = new Order({
+        products,
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+      });
+
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => res.redirect("/orders"))
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutCancel = (req, res, next) => {
+  res.render("error");
 };
 
 exports.postOrder = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -15,6 +15,16 @@ router.get("/orders", middlewareIsAuth, shopController.getOrders);
 router.get("/invoices/:orderId", middlewareIsAuth, shopController.getInvoice);
 router.post("/create-order", middlewareIsAuth, shopController.postOrder);
 router.get("/checkout", middlewareIsAuth, shopController.getCheckout);
+router.get(
+  "/checkout/success",
+  middlewareIsAuth,
+  shopController.getCheckoutSuccess
+);
+router.get(
+  "/checkout/cancel",
+  middlewareIsAuth,
+  shopController.getCheckoutCancel
+);
 router.post(
   "/cart-delete-item",
   middlewareIsAuth,
